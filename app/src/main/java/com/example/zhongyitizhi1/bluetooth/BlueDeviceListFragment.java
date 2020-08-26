6662908340dd60af213d282293d83bde3eb12ba4
118988577 <1018988577@qq.com>
2020-08-26 21:59:15 +0800

    第二次修改
    修改蓝牙扫描方式，增加下拉滑动手势，

--- app/src/main/java/com/example/zhongyitizhi1/bluetooth/BlueDeviceListFragment.java
+++ app/src/main/java/com/example/zhongyitizhi1/bluetooth/BlueDeviceListFragment.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.zhongyitizhi1.R;
 import com.inuker.bluetooth.library.search.SearchRequest;
@@ -17,6 +19,7 @@ import com.inuker.bluetooth.library.search.SearchResult;
 import com.inuker.bluetooth.library.search.response.SearchResponse;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class BlueDeviceListFragment extends Fragment {
@@ -25,11 +28,12 @@ public class BlueDeviceListFragment extends Fragment {
     private BlueDevicelistAdapter mAdapter;
     private Set<SearchResult> mblueDevices;
     private String mBlueToothConnectedMac = "";
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_bluedevice_list, container, false);
-
+        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swiperefresh);
         Intent intent = new Intent(getActivity(),MyBleService.class);
         getActivity().startService(intent);
 
@@ -37,9 +41,29 @@ public class BlueDeviceListFragment extends Fragment {
         mBlueDeviceRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view_device_list);
         mBlueDeviceRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+
+        return view;
+    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         mAdapter = new BlueDevicelistAdapter(getActivity());
         mBlueDeviceRecyclerView.setAdapter(mAdapter);
-        return view;
+        // BEGIN_INCLUDE (setup_refreshlistener)
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //在这里实现数据更新
+                searchDevice();
+                //完成数据更新后，调用setRefreshing(false)。调用此方法可指示 SwipeRefreshLayout 移除进度指示器并更新视图内容。
+            }
+        });
+        // END_INCLUDE (setup_refreshlistener)
+    }
+    private void onRefreshComplete() {
+
+        // Stop the refreshing indicator
+        mSwipeRefreshLayout.setRefreshing(false);
     }
     public void searchDevice() {
         SearchRequest request = new SearchRequest.Builder()
@@ -67,6 +91,8 @@ public class BlueDeviceListFragment extends Fragment {
         @Override
         public void onSearchStopped() {
             ClientManager.getClient().stopSearch();
+            onRefreshComplete();
+            Toast.makeText(getActivity(), "扫描完成", Toast.LENGTH_SHORT).show();
         }
         @Override
         public void onSearchCanceled() {
